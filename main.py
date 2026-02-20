1c1415bb3704ff95e9a7793ea21361dbf63e5b79
Luka <luka.akhalkatsi@makingscience.com>
2026-02-20 12:02:55 +0400

    feat: add main and cloudbuildyaml

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+from extractor.asteroids_extractor import AsteroidsExtractor
+from client.storage_client import GCSClient
+from utils.logger import logger
+from utils.helper import validate_date
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(request):
+    request_json = request.get_json(silent=True)
+
+    start_date = request_json.get("start_date")
+    end_date = request_json.get("end_date")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if not start_date or not end_date:
+        return {"error": "start_date and end_date are required"}, 400
 
+    if not validate_date(start_date) or not validate_date(end_date):
+        return {"error": "Dates must be in YYYY-MM-DD format"}, 400
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    extractor = AsteroidsExtractor(start_date, end_date)
+    client = GCSClient()
+    filename = f"extracted/asteroids_{start_date}_{end_date}.json"
+    try:
+        data = extractor.get_asteroids_data()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if not data:
+            logger.warning("No asteroids data found")
+            return {"message": "No data found"}, 204
+
+        logger.info(f"Uploading asteroid data, file_path: {filename}, record_count: {len(data)}")
+
+        client.upload(data, filename)
+        logger.info("Upload successful")
+        return {"message": "Extract complete", "file_path": filename}
+    except Exception:
+        logger.exception("Asteroid extraction failed")
+        return {"error": "Internal server error"}, 500
