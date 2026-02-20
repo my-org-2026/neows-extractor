5e198d102a83af9519e7912c9f876575c5ffc9ee
Luka <luka.akhalkatsi@makingscience.com>
2026-02-20 15:53:07 +0400

    fix: remove static urls from cloudbuild, add secret manager

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from google.cloud import secretmanager
 
 def validate_date(date_str):
     try:
@@ -7,3 +7,12 @@ def validate_date(date_str):
         return True
     except ValueError:
         return False
+
+
+def access_secret(project_id, secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("utf-8")
--- utils/settings.py
+++ utils/settings.py
@@ -1,11 +1,13 @@
 
 from dotenv import load_dotenv
+from utils.helper import access_secret
 import os
 
 load_dotenv()
 
 class Settings:
-    API_KEY = os.getenv("NASA_API_KEY")
+    PROJECT_ID = os.getenv("PROJECT_ID")
+    API_KEY = access_secret(PROJECT_ID, "NASA_API_KEY")
     API_BASE_URL = os.getenv("NASA_BASE_URL")
     ASTEROIDS_BUCKET = os.getenv("ASTEROIDS_BUCKET")
 
